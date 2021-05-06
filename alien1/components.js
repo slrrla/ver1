7fa0161cf4b35bea3639f49fb0d83e124983144b
slrrla <haha9783125@gmail.com>
2021-05-06 18:24:35 +0900

    dd
    dd

--- alien1/components.js
+++ alien1/components.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import {TouchableOpacity,Text} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React,{Fragment}  from 'react';
+import { StyleSheet, Text, View, ViewPropTypes,Image } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+//import icons,header
+//https://reactnativeelements.com/docs/icon/#component
+import { Icon,Header } from 'react-native-elements';
 
 
 
+const Header= ()=>{
 
-const BottomButton= ()=>{
+  {/*title tab with search and list icons and buttons*/}
+ 
     return(
-        <TouchableOpacity>
-            <Text style ={{fontSize:24}}>Button</Text>
-        </TouchableOpacity>
+        <Header
+        leftComponent = {{text:"홈어",style : styles.TextTitle}}
+        rightComponent = {{icon:"search",color:"#fff",size:30}}
+        containerStyle={{
+          backgroundColor :"#395EC7",
+          alignItems:"flex-end",
+          size :100
+        }}
+  
+        
+      />
     )
 
 }
