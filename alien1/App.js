8092506a4f2ae5f6c5afcb9a1cb24481698b6305
junybaik <82946999+junybaik@users.noreply.github.com>
2021-05-06 18:23:11 +0900

    Create Main.js, Fixed App.js

--- alien1/App.js
+++ alien1/App.js
@@ -1,132 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React,{Fragment}  from 'react';
-import { StyleSheet, Text, View, ViewPropTypes,Image } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { Dimensions } from 'react-native';
+import React from "react";
+import { Alert } from "react-native";
 
-// import BottmTabNavigator to remove space form bottom 
-import { BottomTabBar, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { color } from 'react-native-reanimated';
-import BottomButton from './components';
+export default class extends React.Component {
+	state = {
+		isLoading: true
+	};
 
-
-//import icons
-//https://reactnativeelements.com/docs/icon/#component
-import { Icon,Header } from 'react-native-elements';
-
-
-
-
-
-    
-export default function App() {
-  return (
-
-        
-    <View style={styles.container}>
-      {/*title tab with search and list icons and buttons*/}
- 
-
-    <Header
-      leftComponent = {{text:"홈어",style : styles.TextTitle}}
-      rightComponent = {{icon:"search",color:"#fff",size:30}}
-      containerStyle={{
-        backgroundColor :"#395EC7",
-        alignItems:"flex-end",
-        size :100
-      }}
-    />
-
-      {/*content goes here */}
-      <View style = {styles.Content}>
-        <Text style = {styles.Text}>Content goes in here.</Text>
-      </View>
-
-    {/*toolbar icons and buttons */}
-      <View style = {styles.ToolBar}>
-        <View style = {styles.Tools}>
-          
-          {/*search for valid icon such as fix or tool*/}
-          <Icon
-
-          name = 'menu'
-          color ='#fff'
-          />
-        </View>
-        <View style = {styles.Tools}>
-        <Icon
-          name = 'camera'
-          color ='#fff'
-          />
-        </View>
-        <View style = {styles.Tools}>
-          <Icon
-          name = 'menu'
-          color ='#fff'
-          />
-        </View>
-
-
-
-      </View>
-      <StatusBar style="inverted" />
-      </View>
-
-
-
-
-);
+	render() {
+		const { isLoading } = this.state;
+		return isLoading ? (
+			<Loading />
+		)
+	}
 }
-
-const styles = StyleSheet.create({
-  
-  width: Dimensions.get('screen').width,
-  height: Dimensions.get('screen').height,
-
-
-
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-
-  TopTitle:{
-    flex :1,
-    backgroundColor :"#395EC7",
-    flexDirection:"row",
-    justifyContent:"space-between",
-    alignItems:"center"
-  },
-
-  Content:{
-    flex :15,
-    justifyContent: "center"
-  },
-  TextTitle:{
-    textAlign:"left",
-    fontSize : 30,
-    color :"#fff",
-    alignItems:"center"
-    
-  },
-  ToolBar: {
-    flex: 1,
-    backgroundColor: "#395EC7",
-    flexDirection: "row"
-  },
-  Tools: {
-    flexDirection: "row",
-    flex: 1,
-    justifyContent: "center",
-    paddingVertical: 20
-  },
-  Text: {
-    textAlign: "center",
-    fontSize: 50
-  },
-  ToolTop: {
-
-  }
-  
-});
