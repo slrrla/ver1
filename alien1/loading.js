93a72f330a29b94569587f5701bd1dd1e41568f2
slrrla <haha9783125@gmail.com>
2021-05-06 18:59:55 +0900

    hhhh
    vffff

--- alien1/loading.js
+++ alien1/loading.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View } from "react-native";
 export default function Loading() {
 	return (
 	<View style={styles.container}>
-		<Text style={styles.Text}>Alien</Text>
+		<Text style={styles.text}>Alien</Text>
 	</View>
 	);
 }
@@ -16,9 +16,9 @@ const styles = Stylesheet.create({
 		paddingHorizontal: 30,
 		paddingVertical: 100,
 		backgroundColor: "#add8e6"
-	}
+	},
 	text: {
 		color: "#2c2c2c",
 		fontSize: 30
-	}
+	},
 });	
