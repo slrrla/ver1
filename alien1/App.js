484130d0a7241d81289adbe9c5448d3d4c727975
junybaik <82946999+junybaik@users.noreply.github.com>
2021-05-06 18:51:22 +0900

    Update App.js

--- alien1/App.js
+++ alien1/App.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Alert } from "react-native";
+import Loading from "./Loading";
+import Main from "./Main";
 
 export default class extends React.Component {
 	state = {
 		isLoading: true
 	};
-
+	
+	/*getData = {
+		this.setState({
+			isLoadig: false
+		});
+	}*/
 	render() {
 		const { isLoading } = this.state;
 		return isLoading ? (
 			<Loading />
-		)
+		) : (
+			<Main />
+		);
 	}
 }
